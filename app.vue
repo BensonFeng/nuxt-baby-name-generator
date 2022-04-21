b3d86f742477033c938e451fe9de861f1b1417fc
BensonFeng <bensonfeng@outlook.com>
2022-04-21 17:31:22 -0400

    html content

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" Button below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-button">
+          <button>Boy</button>
+          <button>Unisex</button>
+          <button>Girl</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-button">
+          <button>Trendy</button>
+          <button>Unique</button>
+          <button>Girl</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's lenght</h4>
+        <div class="option-button">
+          <button>Long</button>
+          <button>All</button>
+          <button>short</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
